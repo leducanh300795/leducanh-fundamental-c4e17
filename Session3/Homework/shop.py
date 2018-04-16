4694c3666aa45c60a7773451ca5cdcff6d52a04c
leducanh300795 <leducanh300795@gmail.com>
2018-04-16 22:49:48 +0700

    intial commit

---
+++ Session3/Homework/shop.py
@@ -0,0 +1,23 @@
+items = ['T-Shirt', 'Sweater']
+
+while True:
+
+    print("Welcome to our shop, what do u want?")
+    # print("Our items: ", end = "")
+    # print(*items, sep=", ")
+    #
+    # a = str(input("Enter new iteam: "))
+    # items.append(a)
+    # print("Our items: ", end = "")
+    # print(*items, sep=", ")
+    #
+    # b = int(input("Update position: "))
+    # c = str(input("Input new item: "))
+    # items[b] = c
+    # print("Our items: ", end = "")
+    # print(*items, sep=", ")
+    #
+    # d = int(input("Delete position: "))
+    # del items[d]
+    # print("Our items: ", end ="")
+    # print(*items, sep=", ")
